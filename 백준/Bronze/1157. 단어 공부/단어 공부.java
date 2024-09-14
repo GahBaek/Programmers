0b9f295e33ef2e90a3426bc75f73356eb194298f
백가현 <127074448+GahBaek@users.noreply.github.com>
2024-09-14 16:30:35 +0900

    [Bronze I] Title: 단어 공부, Time: 628 ms, Memory: 129692 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,35 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Locale;
-import java.util.Scanner;
 
 class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine().trim();
 
         HashMap<String, Integer> hashMap = new HashMap<>();
-
         String[] str = input.split("");
         String answer = "";
         int max = 0;
+        boolean isDuplicateMax = false;
+
         for (int i = 0; i < str.length; i++) {
             String string = str[i].toUpperCase(Locale.ROOT);
             hashMap.put(string, hashMap.getOrDefault(string, 0) + 1);
-            if (hashMap.get(string) > max) {
-                max = hashMap.get(string);
+            int count = hashMap.get(string);
+
+            if (count > max) {
+                max = count;
+                answer = string;
+                isDuplicateMax = false;
+            } else if (count == max) {
+                isDuplicateMax = true;
             }
         }
 
-        int maxCount = 0;
-
-        for (String key : hashMap.keySet()) {
-            if (hashMap.get(key) == max) {
-                maxCount++;
-                answer = key;
-            }
-        }
-
-        if (maxCount > 1) {
+        if (isDuplicateMax) {
             answer = "?";
         }
 
