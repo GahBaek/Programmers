478b2a7ebb46368e9d9bfcfb69a01e6a78e8026e
백가현 <127074448+GahBaek@users.noreply.github.com>
2024-12-25 13:00:08 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 26.24 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,21 +1,11 @@
 import java.util.*;
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
-        ArrayList<Integer> list = new ArrayList<>();
-
-        for(int i = 1; i < food.length; i++) {
+        String answer = "0";
+        for(int i = food.length-1; i >=0; i--) {
             for(int j = 0; j < food[i]/2; j++) {
-                list.add(i);
+                answer = i + answer +i;
             }
-        }
-
-        for(int i = 0; i < list.size(); i++) {
-            answer += list.get(i);
-        }
-        answer += "0";
-        for (int i = list.size()-1; i >=0; i--) {
-            answer += list.get(i);
         }
         return answer;
     }
