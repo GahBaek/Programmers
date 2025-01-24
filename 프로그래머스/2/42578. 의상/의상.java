2e5da276793a2dfb15f6d9bf48d9a72516ae337d
백가현 <127074448+GahBaek@users.noreply.github.com>
2025-01-24 12:46:35 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> type = new HashMap<>();
+            for(int i = 0; i < clothes.length; i++) {
+                if (type.containsKey(clothes[i][1])) {
+                    type.put(clothes[i][1], type.getOrDefault(clothes[i][1], 1) + 1);
+                }else{
+                    type.put(clothes[i][1], 1);
+                }
+            }
+
+            Integer[] count = type.values().toArray(new Integer[0]);
+            if(type.size() == 1){
+                answer = type.get(clothes[0][1]);
+            }else{
+                for(int i = 0; i < count.length; i++) {
+                    answer *= count[i] +1;
+                }
+                answer -= 1;
+            }
+        return answer;
+    }
+}
